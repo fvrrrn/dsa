d0d4721e45870e00768b87f7b1ef60097806a077
fvrrrn <boris.chernystrand@gmail.com>
2025-04-25 15:00:20 +0300

    lang(py): add linked list

---
+++ py/linked_list.py
@@ -0,0 +1,95 @@
+from typing import Generic, List, Optional, Protocol, TypeVar
+
+# class Equalable(Protocol):
+#     def __eq__(self, other: object) -> bool:
+#         ...
+#     def __ne__(self, other: object) -> bool:
+#         ...
+
+# T = TypeVar("T", bound=Equalable)
+T = TypeVar("T")
+
+
+class Node(Generic[T]):
+    def __init__(self, v: T) -> None:
+        self.value: T = v
+        self.next: Optional[Node[T]] = None
+        self.prev: Optional[Node[T]] = None
+
+
+class LinkedList(Generic[T]):
+    def __init__(self) -> None:
+        self.head: Optional[Node[T]] = None
+        self.tail: Optional[Node[T]] = None
+        self.size = 0
+
+    def __iter__(self):
+        node = self.head
+        while node is not None:
+            yield node
+            node = node.next
+
+    def __len__(self):
+        return self.size
+
+    def add_in_tail(self, item: Node[T]) -> None:
+        match self.head, self.tail:
+            case None, None:
+                self.head = item
+                self.tail = item
+                # TODO: incorrectly initialized linked list
+            case _, None:
+                pass
+            case None, _:
+                pass
+            case _, _:
+                self.tail.next = item
+                item.prev = self.tail
+                self.tail = item
+
+    def print_all_nodes(self) -> None:
+        for node in self:
+            print(node.value)
+
+    def find(self, val: T) -> Optional[Node[T]]:
+        return next((node for node in self if node.value == val), None)
+
+    def find_all(self, val: T) -> List[Node[T]]:
+        return [node for node in self if node.value == val]
+
+    def delete(self, val: T, all: bool = False) -> None:
+        deleted: List[T] = []
+        for node in self.find_all(val):
+            if self.head is None or self.tail is None:
+                break
+
+            if all is False and node.value in deleted:
+                continue
+            else:
+                deleted.append(node.value)
+
+            match node == self.head, node == self.tail:
+                case True, True:
+                    self.head = None
+                    self.tail = None
+                case True, False:
+                    if headNext := self.head.next:
+                        headNext.prev = None
+                        self.head = headNext
+                case False, True:
+                    if tailPrev := self.tail.prev:
+                        tailPrev.next = None
+                        self.tail = tailPrev
+                case False, False:
+                    if (nodePrev := node.prev) and (nodeNext := node.next):
+                        nodePrev.next = nodeNext
+                        nodeNext.prev = nodePrev
+
+    def clean(self) -> None:
+        pass
+
+    def len(self) -> int:
+        return self.size
+
+    def insert(self, afterNode: Optional[Node[T]], newNode: Node[T]) -> None:
+        pass
---
+++ py/test_linked_list.py
@@ -0,0 +1,128 @@
+import unittest
+from typing import cast
+
+from linked_list import LinkedList, Node
+
+
+class TestLinkedList(unittest.TestCase):
+    def test_add_in_tail(self):
+        ll = LinkedList[int]()
+        node1 = Node(1)
+        node2 = Node(2)
+        ll.add_in_tail(node1)
+        ll.add_in_tail(node2)
+        self.assertEqual(ll.head, node1)
+        self.assertEqual(ll.tail, node2)
+        # self.assertEqual(ll.head.next, node2)
+
+    def test_find(self):
+        ll = LinkedList()
+        node1 = Node(10)
+        node2 = Node(20)
+        ll.add_in_tail(node1)
+        ll.add_in_tail(node2)
+        found = ll.find(20)
+        self.assertEqual(found, node2)
+
+    def test_find_not_found(self):
+        ll = LinkedList()
+        ll.add_in_tail(Node(5))
+        self.assertIsNone(ll.find(99))
+
+    # def test_len(self):
+    #     ll = LinkedList()
+    #     self.assertEqual(ll.len(), 0)
+    #     ll.add_in_tail(Node(1))
+    #     ll.add_in_tail(Node(2))
+    #     self.assertEqual(ll.len(), 2)
+
+    # def test_clean(self):
+    #     ll = LinkedList()
+    #     ll.add_in_tail(Node(1))
+    #     ll.add_in_tail(Node(2))
+    #     ll.clean()
+    #     self.assertIsNone(ll.head)
+    #     self.assertIsNone(ll.tail)
+    #     self.assertEqual(ll.len(), 0)
+    def test_delete_only_node(self):
+        ll = LinkedList[int]()
+        node = Node(10)
+        ll.add_in_tail(node)
+        ll.delete(10)
+        self.assertIsNone(ll.head)
+        self.assertIsNone(ll.tail)
+
+    def test_delete_tail_only(self):
+        ll = LinkedList[int]()
+        ll.add_in_tail(Node(1))
+        node = Node(2)
+        ll.add_in_tail(node)
+        ll.delete(2)
+        self.assertEqual(cast(Node[int], ll.tail).value, 1)
+        self.assertIsNone(cast(Node[int], ll.tail).next)
+
+    def test_delete_head_only(self):
+        ll = LinkedList[int]()
+        node1 = Node(1)
+        ll.add_in_tail(node1)
+        ll.add_in_tail(Node(2))
+        ll.delete(1)
+        self.assertEqual(cast(Node[int], ll.head).value, 2)
+        self.assertIsNone(cast(Node[int], ll.head).prev)
+
+    def test_delete_middle_node(self):
+        ll = LinkedList[int]()
+        ll.add_in_tail(Node(1))
+        middle = Node(2)
+        ll.add_in_tail(middle)
+        ll.add_in_tail(Node(3))
+        ll.delete(2)
+        values = [node.value for node in ll]
+        self.assertEqual(values, [1, 3])
+
+    def test_delete_multiple_all_flag(self):
+        ll = LinkedList[int]()
+        ll.add_in_tail(Node(5))
+        ll.add_in_tail(Node(5))
+        ll.add_in_tail(Node(5))
+        ll.delete(5, all=True)
+        self.assertIsNone(ll.head)
+        self.assertIsNone(ll.tail)
+
+    def test_delete_multiple_all_flag_2(self):
+        ll = LinkedList[int]()
+        node1 = Node(1)
+        ll.add_in_tail(node1)
+        ll.add_in_tail(Node(5))
+        ll.add_in_tail(Node(5))
+        ll.add_in_tail(Node(5))
+        ll.delete(5, all=True)
+        self.assertEqual(ll.head, node1)
+        self.assertEqual(ll.tail, node1)
+
+    def test_delete_multiple_all_flag_3(self):
+        ll = LinkedList[int]()
+        node1 = Node(1)
+        node2 = Node(2)
+        ll.add_in_tail(node1)
+        ll.add_in_tail(node2)
+        ll.add_in_tail(Node(5))
+        ll.add_in_tail(Node(5))
+        ll.add_in_tail(Node(5))
+        ll.delete(5, all=True)
+        self.assertEqual(ll.head, node1)
+        self.assertEqual(ll.tail, node2)
+
+    def test_delete_first_occurrence_only(self):
+        ll = LinkedList[int]()
+        ll.add_in_tail(Node(1))
+        ll.add_in_tail(Node(2))
+        ll.add_in_tail(Node(2))
+        ll.add_in_tail(Node(3))
+        ll.delete(2)
+        values = [node.value for node in ll]
+        self.assertEqual(values, [1, 2, 3])
+
+
+if __name__ == "__main__":
+    unittest.main()
