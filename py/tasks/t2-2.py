6317a9b1a0f20f1ff43abd62cddb4a015ac75a9d
fvrrrn <boris.chernystrand@gmail.com>
2025-04-27 18:41:19 +0300

    lang(py): add 2.11 has cycles

--- py/tasks/t2-2.py
+++ py/tasks/t2-2.py
@@ -10,3 +10,12 @@ def t2_10(l: LinkedList2[T]) -> LinkedList2[T]:
     for node in reversed(l):
         r.add_in_tail(Node(node.value))
     return r
+
+
+def t2_11(l: LinkedList2[T]) -> bool:
+    node = l.head
+    for _ in range(l.size):
+        if node is None:
+            return False
+        node = node.next
+    return node is not None
