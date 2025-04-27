cebe424c7d249220c6dfc54a24ad322f753af473
fvrrrn <boris.chernystrand@gmail.com>
2025-04-27 18:20:56 +0300

    lang(py): add 2.10 reverselist

---
+++ py/tasks/t2-2.py
@@ -0,0 +1,12 @@
+from typing import TypeVar
+
+from t2 import LinkedList2, Node
+
+T = TypeVar("T")
+
+
+def t2_10(l: LinkedList2[T]) -> LinkedList2[T]:
+    r = LinkedList2()
+    for node in reversed(l):
+        r.add_in_tail(Node(node.value))
+    return r
