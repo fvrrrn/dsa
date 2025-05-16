aba941e0998d6b7f27c48e1a6dbec52ee152ffb5
fvrrrn <boris.chernystrand@gmail.com>
2025-05-16 22:45:08 +0300

    lang(py): add dummy node to linked list

--- py/linked_list.py
+++ py/linked_list.py
@@ -1,94 +1,81 @@
-import threading
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor, as_completed
-from typing import (
-    Generic,
-    Iterator,
-    List,
-    Optional,
-    Protocol,
-    TypeGuard,
-    TypeVar,
-    Union,
-    cast,
-)
+from typing import Generic, Iterator, List, Optional, TypeVar
 
 from monads import Just, Maybe, Nothing
-from protocols import Comparable
 
-T = TypeVar("T", bound=Comparable)
+T = TypeVar("T")
 
 
 class Node(Generic[T]):
     def __init__(self, v: T) -> None:
         self.value: T = v
-        self.next: Optional[Node[T]] = None
-        self.prev: Optional[Node[T]] = None
+        self.next: Node[T] | Dummy
+        self.prev: Node[T] | Dummy
 
-    def __str__(self):
-        return str(self.value)
-
-
-class HasNext(Protocol, Generic[T]):
-    value: T
-    prev: Optional[Node[T]]
-    next: Node[T]
+    def __repr__(self) -> str:
+        return f"Node({self.value})"
 
 
-class HasPrev(Protocol, Generic[T]):
-    value: T
-    prev: Node[T]
-    next: Optional[Node[T]]
+class Dummy(Node):
+    def __init__(self) -> None:
+        super().__init__(None)
 
+    def __repr__(self) -> str:
+        return "Dummy()"
 
-class HasPrevNext(Protocol, Generic[T]):
-    value: T
-    prev: Node[T]
-    next: Node[T]
-
-
-def is_head(node: Node[T]) -> TypeGuard[HasNext[T]]:
-    return node.prev is None
-
-
-def is_tail(node: Node[T]) -> TypeGuard[HasPrev[T]]:
-    return node.next is None
-
-
-def is_middle(node: Node[T]) -> TypeGuard[HasPrevNext[T]]:
-    return node.prev is not None and node.next is not None
+    def __str__(self) -> str:
+        return "Dummy"
 
 
 class LinkedList(Generic[T]):
     def __init__(self, *values: T) -> None:
-        # TODO: make fields read-only so that there won't be erroneous states
-        self.head: Optional[Node[T]] = None
-        self.tail: Optional[Node[T]] = None
-        self.size = 0
+        self._head = Dummy()
+        self._tail = Dummy()
+        self._head.next = self._tail
+        self._tail.prev = self._head
+        self._size = 0
         for value in values:
-            self.add_in_tail(value)
+            self.append(value)
+
+    @property
+    def head(self) -> Optional[T]:
+        match self._head.next:
+            case Dummy():
+                return None
+            case Node():
+                return self._head.next.value
 
     def __iter__(self) -> Iterator[Node[T]]:
-        node = self.head
-        while node is not None:
-            yield node
-            node = node.next
+        node = self._head.next
+        # prevent infinite loop if cycles are present
+        for _ in range(self._size):
+            match node:
+                case Dummy():
+                    break
+                case Node():
+                    yield node
+                    node = node.next
 
     def __len__(self):
-        return self.size
+        return self._size
 
     def __str__(self):
         return " -> ".join(str(node.value) for node in self)
 
     def __reversed__(self):
-        node = self.tail
-        while node is not None:
-            yield node
-            node = node.prev
+        node = self._tail.prev
+        # prevent infinite loop if cycles are present
+        for _ in range(self._size):
+            match node:
+                case Dummy():
+                    break
+                case Node():
+                    yield node
+                    node = node.prev
 
     def __getitem__(self, index: int) -> Maybe[Node[T]]:
-        if not (0 <= index < self.size):
+        if not (0 <= index < self._size):
             return Nothing()
-        node = self.head
+        node = self._head
         for _ in range(index):
             if node is not None:
                 node = node.next
@@ -96,45 +83,23 @@ class LinkedList(Generic[T]):
             return Nothing()
         return Just(node)
 
-    def add_in_head(self, item: Union[Node[T], T]) -> None:
-        if not isinstance(item, Node):
-            item = Node(item)
-        match self.head, self.tail:
-            case None, None:
-                self.head = item
-                self.tail = item
-                self.size += 1
-            case _, None:
-                pass
-            case None, _:
-                pass
-            case _, _:
-                item.next = self.head
-                self.head.prev = item
-                self.head = item
-                self.size += 1
+    def prepend(self, value: T) -> Node[T]:
+        node = Node(value)
+        node.prev = self._head
+        node.next = self._head.next
+        self._head.next.prev = node
+        self._head.next = node
+        self._size += 1
+        return node
 
-    def add_in_tail(self, item: Union[Node[T], T]) -> None:
-        if not isinstance(item, Node):
-            item = Node(item)
-        match self.head, self.tail:
-            case None, None:
-                self.head = item
-                self.tail = item
-                self.size += 1
-            case _, None:
-                pass
-            case None, _:
-                pass
-            case _, _:
-                self.tail.next = item
-                item.prev = self.tail
-                self.tail = item
-                self.size += 1
-
-    def print_all_nodes(self) -> None:
-        for node in self:
-            print(node.value)
+    def append(self, value: T) -> Node[T]:
+        node = Node(value)
+        node.prev = self._tail.prev
+        node.next = self._tail
+        self._tail.prev.next = node
+        self._tail.prev = node
+        self._size += 1
+        return node
 
     def find(self, val: T) -> Optional[Node[T]]:
         return next((node for node in self if node.value == val), None)
@@ -144,157 +109,13 @@ class LinkedList(Generic[T]):
 
     def delete(self, val: T, all: bool = False) -> None:
         for node in self.find_all(val):
-            if self.head is None or self.tail is None:
-                break
-            match node == self.head, node == self.tail:
-                case True, True:
-                    self.head = None
-                    self.tail = None
-                case True, False:
-                    if headNext := self.head.next:
-                        headNext.prev = None
-                        self.head = headNext
-                case False, True:
-                    if tailPrev := self.tail.prev:
-                        tailPrev.next = None
-                        self.tail = tailPrev
-                case False, False:
-                    if (nodePrev := node.prev) and (nodeNext := node.next):
-                        nodePrev.next = nodeNext
-                        nodeNext.prev = nodePrev
-            self.size -= 1
+            node.prev.next = node.next
+            node.next.prev = node.prev
+            self._size -= 1
             if all is False:
                 return
 
     def clean(self) -> None:
-        self.head = None
-        self.tail = None
-        self.size = 0
-
-    def len(self) -> int:
-        return self.size
-
-    def insert(self, afterNode: Optional[Node[T]], newNode: Node[T]) -> None:
-        match self.head, self.tail, afterNode:
-            case None, None, None:
-                self.add_in_tail(newNode)
-            case None, _, _:
-                pass
-            case _, None, _:
-                pass
-            case _, _, None:
-                self.tail.next = newNode
-                newNode.prev = self.tail
-                self.tail = newNode
-                self.size += 1
-            case _, _, _:
-                # TypeGuard does not work with match-case
-                if is_tail(afterNode):
-                    afterNode.next = newNode
-                    newNode.prev = cast(Node[T], afterNode)
-                    self.size += 1
-                    self.tail = newNode
-                elif is_head(afterNode) or is_middle(afterNode):
-                    afterNode.next.prev = newNode
-                    newNode.next = afterNode.next
-                    afterNode.next = newNode
-                    newNode.prev = cast(Node[T], afterNode)
-                    self.size += 1
-
-
-def _partition(low: HasNext[T], high: HasPrev[T]) -> Node[T]:
-    pivot = high
-    i = cast(HasNext[T], low.prev)
-    j = low
-
-    while j != high:
-        if j.value < pivot.value:
-            i = cast(HasNext[T], i.next) if i else low
-            i.value, j.value = j.value, i.value
-        j = cast(HasNext[T], j.next)
-    i = i.next if i else low
-    i.value, pivot.value = pivot.value, i.value
-    return cast(Node[T], i)
-
-
-def _threaded_quick_sort_recursive(low: Node[T], high: Node[T]) -> None:
-    if low and high and low != high and low != high.next:
-        pivot = _partition(cast(HasNext[T], low), cast(HasPrev[T], high))
-        left_thread = threading.Thread(
-            target=_threaded_quick_sort_recursive, args=(low, pivot.prev)
-        )
-        right_thread = threading.Thread(
-            target=_threaded_quick_sort_recursive, args=(pivot.next, high)
-        )
-
-        left_thread.start()
-        right_thread.start()
-
-        left_thread.join()
-        right_thread.join()
-
-
-def _quick_sort_recursive(low: Node[T], high: Node[T]) -> None:
-    if low and high and low != high and low != high.next:
-        pivot = _partition(cast(HasNext[T], low), cast(HasPrev[T], high))
-        _quick_sort_recursive(low, pivot.prev)  # type: ignore
-        _quick_sort_recursive(pivot.next, high)  # type: ignore
-
-
-def _threaded_quick_sort_recursive_2(
-    low: Node[T], high: Node[T], pool: ThreadPoolExecutor, threshold: int = 10
-) -> None:
-    if low and high and low != high and low != high.next:
-        pivot = _partition(cast(HasNext[T], low), cast(HasPrev[T], high))
-        if _partition_size(low, high) > threshold:
-            futures = [
-                pool.submit(_threaded_quick_sort_recursive_2, low, pivot.prev, pool, threshold),  # type: ignore
-                pool.submit(_threaded_quick_sort_recursive_2, pivot.next, high, pool, threshold),  # type: ignore
-            ]
-
-            for future in as_completed(futures):
-                future.result()
-        else:
-            _threaded_quick_sort_recursive_2(low, pivot.prev, pool, threshold)  # type: ignore
-            _threaded_quick_sort_recursive_2(pivot.next, high, pool, threshold)  # type: ignore
-
-
-def _partition_size(low: Node[T], high: Node[T]) -> int:
-    size = 0
-    current = low
-    while current != high.next:
-        size += 1
-        current = current.next  # type: ignore
-    return size
-
-
-def _parallel_quick_sort_recursive(
-    low: Node[T], high: Node[T], pool: ProcessPoolExecutor
-) -> None:
-    if low and high and low != high and low != high.next:
-        pivot = _partition(cast(HasNext[T], low), cast(HasPrev[T], high))
-        left_task = pool.submit(_parallel_quick_sort_recursive, low, pivot.prev, pool)  # type: ignore
-        right_task = pool.submit(_parallel_quick_sort_recursive, pivot.next, high, pool)  # type: ignore
-        left_task.result()
-        right_task.result()
-
-
-def sort_linked_list(llist: LinkedList[T]) -> LinkedList[T]:
-    sorted_list = LinkedList[T]()
-    for node in llist:
-        # TODO: if node.value is not a primitive it should be deep-copied
-        sorted_list.add_in_tail(node.value)
-    if (
-        llist.head is None
-        or sorted_list.head is None
-        or llist.tail is None
-        or sorted_list.tail is None
-    ):
-        return llist
-    # _threaded_quick_sort_recursive(sorted_list.head, sorted_list.tail)
-    # with ProcessPoolExecutor() as pool:
-    #     _parallel_quick_sort_recursive(sorted_list.head, sorted_list.tail, pool)
-    with ThreadPoolExecutor() as pool:
-        _threaded_quick_sort_recursive_2(sorted_list.head, sorted_list.tail, pool)
-    # _quick_sort_recursive(sorted_list.head, sorted_list.tail)
-    return sorted_list
+        self._head.next = self._tail
+        self._tail.prev = self._head
+        self._size = 0
--- py/test_linked_list.py
+++ py/test_linked_list.py
@@ -1,288 +1,70 @@
 import unittest
-from typing import cast
 
-from linked_list import LinkedList, Node, sort_linked_list
+from linked_list import LinkedList
 
 
 class TestLinkedList(unittest.TestCase):
-    def test_add_in_tail(self):
+
+    def test_append(self):
         ll = LinkedList[int]()
-        node1 = Node(1)
-        node2 = Node(2)
-        ll.add_in_tail(node1)
-        ll.add_in_tail(node2)
-        self.assertEqual(ll.head, node1)
-        self.assertEqual(ll.tail, node2)
-        # will not ever throw
-        assert ll.head is not None
-        self.assertEqual(ll.head.next, node2)
+        ll.append(1)
+        ll.append(2)
+        self.assertEqual(ll.head, 1)
+        self.assertEqual(ll._tail.prev.value, 2)
+
+    def test_prepend(self):
+        ll = LinkedList[int]()
+        ll.prepend(1)
+        ll.prepend(2)
+        self.assertEqual(ll.head, 2)
+        self.assertEqual(ll._tail.prev.value, 1)
 
     def test_find(self):
-        ll = LinkedList()
-        node1 = Node(10)
-        node2 = Node(20)
-        ll.add_in_tail(node1)
-        ll.add_in_tail(node2)
-        found = ll.find(20)
-        self.assertEqual(found, node2)
+        ll = LinkedList[int](1, 2, 3)
+        node = ll.find(2)
+        self.assertIsNotNone(node)
+        self.assertEqual(node.value, 2)  # type: ignore
 
     def test_find_not_found(self):
-        ll = LinkedList()
-        ll.add_in_tail(Node(5))
-        self.assertIsNone(ll.find(99))
+        ll = LinkedList[int](1, 2, 3)
+        node = ll.find(4)
+        self.assertIsNone(node)
 
-    def test_len(self):
-        ll = LinkedList()
-        self.assertEqual(ll.len(), 0)
-        ll.add_in_tail(Node(1))
-        ll.add_in_tail(Node(2))
-        self.assertEqual(ll.len(), 2)
+    def test_find_all(self):
+        ll = LinkedList[int](1, 2, 1, 3)
+        nodes = ll.find_all(1)
+        self.assertEqual(len(nodes), 2)
+        self.assertEqual(nodes[0].value, 1)
+        self.assertEqual(nodes[1].value, 1)
+
+    def test_delete(self):
+        ll = LinkedList[int](1, 2, 3, 2)
+        ll.delete(2)
+        self.assertEqual([node.value for node in ll], [1, 3, 2])
+
+    def test_delete_all(self):
+        ll = LinkedList[int](1, 2, 3, 2)
+        ll.delete(2, all=True)
+        self.assertEqual([node.value for node in ll], [1, 3])
 
     def test_clean(self):
-        ll = LinkedList()
-        ll.add_in_tail(Node(1))
-        ll.add_in_tail(Node(2))
+        ll = LinkedList[int](1, 2, 3)
         ll.clean()
+        self.assertEqual(len(ll), 0)
         self.assertIsNone(ll.head)
-        self.assertIsNone(ll.tail)
-        self.assertEqual(ll.len(), 0)
 
-    def test_delete_only_node(self):
-        ll = LinkedList[int]()
-        node = Node(10)
-        ll.add_in_tail(node)
-        ll.delete(10)
-        self.assertIsNone(ll.head)
-        self.assertIsNone(ll.tail)
+    def test_len(self):
+        ll = LinkedList[int](1, 2, 3)
+        self.assertEqual(len(ll), 3)
 
-    def test_delete_tail_only(self):
-        ll = LinkedList[int]()
-        ll.add_in_tail(Node(1))
-        node = Node(2)
-        ll.add_in_tail(node)
-        ll.delete(2)
-        self.assertEqual(cast(Node[int], ll.tail).value, 1)
-        self.assertIsNone(cast(Node[int], ll.tail).next)
+    def test_str(self):
+        ll = LinkedList[int](1, 2, 3)
+        self.assertEqual(str(ll), "1 -> 2 -> 3")
 
-    def test_delete_head_only(self):
-        ll = LinkedList[int]()
-        node1 = Node(1)
-        ll.add_in_tail(node1)
-        ll.add_in_tail(Node(2))
-        ll.delete(1)
-        self.assertEqual(cast(Node[int], ll.head).value, 2)
-        self.assertIsNone(cast(Node[int], ll.head).prev)
-
-    def test_delete_middle_node(self):
-        ll = LinkedList[int]()
-        ll.add_in_tail(Node(1))
-        middle = Node(2)
-        ll.add_in_tail(middle)
-        ll.add_in_tail(Node(3))
-        ll.delete(2)
-        values = [node.value for node in ll]
-        self.assertEqual(values, [1, 3])
-
-    def test_delete_multiple_all_flag(self):
-        ll = LinkedList[int]()
-        ll.add_in_tail(Node(5))
-        ll.add_in_tail(Node(5))
-        ll.add_in_tail(Node(5))
-        ll.delete(5, all=True)
-        self.assertIsNone(ll.head)
-        self.assertIsNone(ll.tail)
-
-    def test_delete_multiple_all_flag_2(self):
-        ll = LinkedList[int]()
-        node1 = Node(1)
-        ll.add_in_tail(node1)
-        ll.add_in_tail(Node(5))
-        ll.add_in_tail(Node(5))
-        ll.add_in_tail(Node(5))
-        ll.delete(5, all=True)
-        self.assertEqual(ll.head, node1)
-        self.assertEqual(ll.tail, node1)
-
-    def test_delete_multiple_all_flag_3(self):
-        ll = LinkedList[int]()
-        node1 = Node(1)
-        node2 = Node(2)
-        ll.add_in_tail(node1)
-        ll.add_in_tail(node2)
-        ll.add_in_tail(Node(5))
-        ll.add_in_tail(Node(5))
-        ll.add_in_tail(Node(5))
-        ll.delete(5, all=True)
-        self.assertEqual(ll.head, node1)
-        self.assertEqual(ll.tail, node2)
-
-    def test_delete_first_occurrence_only(self):
-        ll = LinkedList[int]()
-        ll.add_in_tail(Node(1))
-        ll.add_in_tail(Node(2))
-        ll.add_in_tail(Node(2))
-        ll.add_in_tail(Node(3))
-        ll.delete(2)
-        values = [node.value for node in ll]
-        self.assertEqual(values, [1, 2, 3])
-
-    def test_insert_into_empty_list(self):
-        ll = LinkedList[int]()
-        node = Node(1)
-        ll.insert(None, node)
-        self.assertEqual(ll.head, node)
-        self.assertEqual(ll.tail, node)
-        self.assertIsNone(node.prev)
-        self.assertIsNone(node.next)
-
-    def test_insert_when_after_is_none(self):
-        ll = LinkedList[int]()
-        node1 = Node(1)
-        ll.add_in_tail(node1)
-        node2 = Node(2)
-        ll.insert(None, node2)
-        self.assertEqual(ll.tail, node2)
-        self.assertEqual(node2.prev, node1)
-        self.assertEqual(node1.next, node2)
-        self.assertEqual(ll.head, node1)
-        self.assertIsNone(node2.next)
-        self.assertEqual(ll.len(), 2)
-
-    def test_insert_after_head(self):
-        ll = LinkedList[int]()
-        node1 = Node(1)
-        node2 = Node(2)
-        ll.add_in_tail(node1)
-        ll.insert(node1, node2)
-        self.assertEqual(node1.next, node2)
-        self.assertEqual(node2.prev, node1)
-        self.assertEqual(ll.tail, node2)
-        self.assertEqual(ll.head, node1)
-
-    def test_insert_in_middle(self):
-        ll = LinkedList[int]()
-        node1 = Node(1)
-        node2 = Node(2)
-        node3 = Node(3)
-        ll.add_in_tail(node1)
-        ll.add_in_tail(node3)
-        ll.insert(node1, node2)
-        self.assertEqual(node1.next, node2)
-        self.assertEqual(node2.prev, node1)
-        self.assertEqual(node2.next, node3)
-        self.assertEqual(node3.prev, node2)
-
-    def test_insert_after_tail(self):
-        ll = LinkedList[int]()
-        node1 = Node(1)
-        ll.add_in_tail(node1)
-        node2 = Node(2)
-        ll.insert(node1, node2)
-        self.assertEqual(ll.head, node1)
-        self.assertEqual(ll.tail, node2)
-        self.assertEqual(node1.next, node2)
-        self.assertEqual(node2.prev, node1)
-        self.assertIsNone(node2.next)
-
-    def test_insert_middle(self):
-        ll = LinkedList[str]()
-        a1 = Node("a1")
-        a2 = Node("a2")
-        a3 = Node("a3")
-        a4 = Node("a4")
-        a5 = Node("a5")
-        a7 = Node("a7")
-
-        ll.add_in_tail(a1)
-        ll.add_in_tail(a2)
-        ll.add_in_tail(a3)
-        ll.add_in_tail(a4)
-        ll.add_in_tail(a5)
-
-        ll.insert(a3, a7)
-
-        values = [node.value for node in ll]
-        self.assertEqual(values, ["a1", "a2", "a3", "a7", "a4", "a5"])
-
-    def test_add_in_head_empty_list(self):
-        ll = LinkedList[int]()
-        node = Node(1)
-        ll.add_in_head(node)
-        self.assertIs(ll.head, node)
-        self.assertIs(ll.tail, node)
-        self.assertEqual(ll.size, 1)
-        self.assertIsNone(node.prev)
-        self.assertIsNone(node.next)
-
-    def test_add_in_head_single_node_list(self):
-        ll = LinkedList[int]()
-        first = Node(1)
-        ll.add_in_tail(first)
-
-        second = Node(0)
-        ll.add_in_head(second)
-
-        self.assertIs(ll.head, second)
-        self.assertIs(ll.tail, first)
-        self.assertEqual(ll.size, 2)
-        self.assertIs(second.next, first)
-        self.assertIsNone(second.prev)
-        self.assertIs(first.prev, second)
-        self.assertIsNone(first.next)
-
-    def test_add_in_head_multiple_nodes_list(self):
-        ll = LinkedList[int]()
-        first = Node(2)
-        second = Node(3)
-        ll.add_in_tail(first)
-        ll.add_in_tail(second)
-
-        new_head = Node(1)
-        ll.add_in_head(new_head)
-
-        self.assertIs(ll.head, new_head)
-        self.assertEqual(ll.size, 3)
-        self.assertIs(new_head.next, first)
-        self.assertIsNone(new_head.prev)
-        self.assertIs(first.prev, new_head)
-        self.assertIs(first.next, second)
-        self.assertIs(second.prev, first)
-        self.assertIsNone(second.next)
-
-    def test_linked_list_initialization(self):
-        llist = LinkedList(5, 3, 8, 1, 4)
-        self.assertEqual(str(llist), "5 -> 3 -> 8 -> 1 -> 4")
-
-    def test_sort_linked_list(self):
-        llist = LinkedList(5, 3, 8, 1, 4)
-        sorted_llist = sort_linked_list(llist)
-        self.assertEqual(str(sorted_llist), "1 -> 3 -> 4 -> 5 -> 8")
-
-    def test_empty_list(self):
-        llist = LinkedList()
-        sorted_llist = sort_linked_list(llist)
-        self.assertEqual(str(sorted_llist), "")
-
-    def test_single_element(self):
-        llist = LinkedList(42)
-        sorted_llist = sort_linked_list(llist)
-        self.assertEqual(str(sorted_llist), "42")
-
-    def test_sorted_list(self):
-        llist = LinkedList(1, 2, 3, 4, 5)
-        sorted_llist = sort_linked_list(llist)
-        self.assertEqual(str(sorted_llist), "1 -> 2 -> 3 -> 4 -> 5")
-
-    def test_reverse_sorted_list(self):
-        llist = LinkedList(5, 4, 3, 2, 1)
-        sorted_llist = sort_linked_list(llist)
-        self.assertEqual(str(sorted_llist), "1 -> 2 -> 3 -> 4 -> 5")
-
-    def test_original_list_mutation(self):
-        original_list = LinkedList(5, 3, 8, 1, 4)
-        original_str = str(original_list)
-        sort_linked_list(original_list)
-        self.assertEqual(original_str, str(original_list))
+    def test_reverse(self):
+        ll = LinkedList[int](1, 2, 3)
+        reversed_values = [node.value for node in reversed(ll)]
+        self.assertEqual(reversed_values, [3, 2, 1])
 
 
 if __name__ == "__main__":
