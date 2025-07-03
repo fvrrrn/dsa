b62945ba80ebb8f45d826181a280b6f2f4f1efc2
fvrrrn <boris.chernystrand@gmail.com>
2025-07-03 18:56:48 +0300

    lang(py): add Either monad

--- py/monads.py
+++ py/monads.py
@@ -2,6 +2,8 @@ from dataclasses import dataclass
 from typing import Generic, TypeVar
 
 T = TypeVar("T")
+L = TypeVar("L")
+R = TypeVar("R")
 
 
 class Maybe(Generic[T]):
@@ -15,4 +17,18 @@ class Just(Maybe[T]):
 
 @dataclass(frozen=True)
 class Nothing(Maybe[T]):
+    __match_args__ = ()  # TODO: not working: allow match-case to work cleanly
+
+
+class Either(Generic[L, R]):
     pass
+
+
+@dataclass(frozen=True)
+class Left(Either[L, R]):
+    value: L
+
+
+@dataclass(frozen=True)
+class Right(Either[L, R]):
+    value: R
